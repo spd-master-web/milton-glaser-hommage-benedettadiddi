081e5368d344439833505d0cc4253e63a3cc7a15
benedetta <diddib@mail.scuoladesign.com>
2020-07-08 14:22:52 +0200

    poster

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-gsap.from(".text-1", { opacity: 0});
-gsap.to(".text-1", {opacity: 1 delay: });
+/*
+gsap.from(".text-1", { opacity: 0, duration:5});
+gsap.to(".text-1", { opacity: 1 });
+
+gsap.from(".text-2", { opacity: 0, duration:5, delay:6});
+gsap.to(".text-2", { opacity: 1 });
+*/
+
+gsap.set(".poster", {scale:0.5});
+gsap.timeline()
+    .from(".poster", {duration:1, opacity:0})
+    .from(".text", {opacity:0, scale:0, ease:"back"})
+    .from(".paragraph", {x:900, duration:2})
